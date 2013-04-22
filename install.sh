4b7649e8177e9585ce62c8ef1f20aea33387a543
sshaw <skye.shaw@gmail.com>
2013-04-21 22:40:24 -0400

    add mysql pager, added --skip option to installer

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 self=$(basename "$0")
+skip=${1##--skip=}
+
 git ls-files | grep -v "$self" | while read f
 do
     dest="$HOME/.$f"
     if [ ! -f "$dest" ] || ! diff "$f" "$dest" > /dev/null
     then
+	[ -n "$skip" ] && [[ "$f" =~ $skip ]] && continue
+
 	echo -e "Copying \0033[1m$f\0033[0m to \0033[1m$dest\0033[0m"
         cp "$f" "$dest" || break
     fi
