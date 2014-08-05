882165d337ae036fa7463240cf3e446252588047
sshaw <skye.shaw@gmail.com>
2014-08-04 21:06:39 -0400

    osx.sh additions

--- osx.sh
+++ osx.sh
@@ -1,5 +1,19 @@
+# Don't use natural scrolling, false means use good ol' scrolling
+defaults write "Apple Global Domain" com.apple.swipescrolldirection -bool false
+
+# No .DS_Store files on network volumes
+# defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+defaults write com.apple.dock autohide -bool true
+
+defaults write com.apple.finder EmptyTrashSecurely -bool true
+# Default Finder view mode, use list view
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
 # Google update
 defaults write com.google.Keystone.Agent checkInterval -int 0
 
-defaults write com.apple.finder EmptyTrashSecurely -bool true
-defaults write com.apple.dock autohide -bool true
+
+
+
