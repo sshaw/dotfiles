0dc9575a44ec56d5ef20988670e3323bd1e60bf6
sshaw <skye.shaw@gmail.com>
2016-04-18 16:20:33 -0400

    Updates to Rails configs

--- rails-template.rb
+++ rails-template.rb
@@ -1,21 +1,11 @@
 gem "foreigner"
 gem "capistrano"
 gem "haml"
-gem "shoulda"
+gem "shoulda-matchers"
 gem "factory_girl"
-gem "formtastic"
 gem "will_paginate"
 
 environment "config.active_record.timestamped_migrations = false"
-environment "config.time_zone = 'Pacific Time (US & Canada)'"
+environment "config.time_zone = 'Eastern Time (US & Canada)'"
 
-run "mkdir test/factories"
-run "rm public/index.html"
-# older versions only
-# run "rm public/images/rails.png"
-
-# newer versions
 run "rm README.rdoc"
-run "rm app/assets/images/rails.png"
-
-
