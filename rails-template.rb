9cf1190e5c752499e41e64f60d3bea2925dad476
sshaw <skye.shaw@gmail.com>
2016-07-24 12:46:20 -0400

    Update Rails config

--- rails-template.rb
+++ rails-template.rb
@@ -1,11 +1,17 @@
-gem "foreigner"
-gem "capistrano"
-gem "haml"
-gem "shoulda-matchers"
-gem "factory_girl"
-gem "will_paginate"
+# Use single quotes to match the Gemfile Rails generates
+gem 'capistrano-rails', :group => 'development'
+gem 'foreigner'
+gem 'kaminari'
+gem 'shoulda-matchers', :group => 'test'
+
+gem_group :development, :test do
+  gem 'factory_girl'
+  gem 'rspec-rails'
+end
 
 environment "config.active_record.timestamped_migrations = false"
 environment "config.time_zone = 'Eastern Time (US & Canada)'"
 
 run "rm README.rdoc"
+
+generate "rspec:install"
